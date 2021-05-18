d4c68899775502c1d5e7eac76009981c544b4056
Konstantin <trigger.kst@gmail.com>
2021-05-18 13:29:32 +0600

    info alert
    Took 1 hour 44 minutes

--- src/js/main.js
+++ src/js/main.js
@@ -31,3 +31,66 @@ tl.fromTo(shapes, {drawSVG:"0%", fill:"#A80010", stroke:"#A80010"}, {duration: 1
     .to(shapes, {duration: 0.5, drawSVG:"90% 100%", fill:"#ffffff", stroke: "#A80010", stagger: 0.5})
     .to(shapes, {duration: 2, scale:1, drawSVG:"100%", stroke:"#A80010", fill:"#ffffff",  transformOrigin:"50% 50%"})
     .to(shapes, {duration: 0.5, fill:"#A80010", scale:1, opacity:1,stroke:"#A80010", strokeWidth:"3", stagger: 0.2,})
+
+
+/*******************/
+/*****Notifier*******/
+/*******************/
+//Собственный модуль уведомлений
+const notify = function (message, type = "default", html = "") { // type может быть success (по умолчанию) или error
+    let rnd = "alert-" + Math.floor(Math.random() * 10000); //
+    jQuery("body").append(`<div class='alert ${type} ${rnd}'>${message} ${html}<span class="closebtn"></span></div>`) // вставляем алерт в дом
+    setTimeout(function () {
+        jQuery("body").find("." + rnd + "").remove();
+    }, 5000);
+}
+jQuery(document).on('click', '.closebtn', function () { // кнопка закрытия алерта
+    let $alert = jQuery(this).parent();
+    $alert.css({"opacity": "0"});
+    setTimeout(function () {
+        $alert.css("display", "none")
+    }, 100);
+})
+const delete_notify = function (input) { // функция "мягкого" скрытия алертов (с анимацией). В качестве input передаем ссылку на элемент, у которого надо убрать класс input-error
+    jQuery('.alert').each(function () {
+        let $alert = jQuery(this);
+        $alert.css({"opacity": "0", "height": "1px"});
+        setTimeout(function () {
+            $alert.remove();
+        }, 600);
+    })
+    if (input) {
+        input.removeClass("input-error");
+    }
+}
+const harddelete_notify = function (input) { // тоже самое, только скрытие всех алертов без анимации (например, сркыть все алерты перед проверкой и в случае необходимости отобразить новый)
+    jQuery('.alert').each(function () {
+        jQuery(this).remove();
+    })
+    if (input) {
+        input.removeClass("input-error");
+    }
+}
+/****************/
+/****************/
+/****************/
+
+
+//закрываем попап алерт СF7
+$('body').on("click", ".wpcf7-response-output", function () {
+    $(this).css("display", "none");
+})
+
+document.addEventListener('wpcf7mailsent', function (event) {
+    $.fancybox.close('true');
+    notify("Ваше сообщение успешно отправлено! Менеджер перезвонит Вам в течение 15 минут", "success");
+}, false);
+document.addEventListener('wpcf7invalid', function (event) {
+    notify("Проверьте все поля!", "error");
+}, false);
+document.addEventListener('wpcf7spam', function (event) {
+    notify("Сообщение выглядит как спам!", "error");
+}, false);
+document.addEventListener('wpcf7mailfailed', function (event) {
+    notify("Не удалось отправить сообщение! Попробуйте позднее!", "error");
+}, false);
